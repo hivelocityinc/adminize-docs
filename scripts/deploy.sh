fbec6311566f5750930969cabc902b9ab8b2dd87
Ryuichi Komeda <komeda@hivelocity.co.jp>
2016-06-13 17:19:14 +0900

    Fix project path when harp compiling

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GITHUB_TOKEN@github
 
 echo "Compiling new static content"
 mkdir $TEMP_DIRECTORY || exit 1
-harp compile public $TEMP_DIRECTORY || exit 1
+harp compile . $TEMP_DIRECTORY || exit 1
 cp .gitignore $TEMP_DIRECTORY || exit 1
 
 echo "Checking out gh-pages branch"
